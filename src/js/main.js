66a6ee9a1f9bca839f82183eab0547e79e39dc0c
ParkYongSoo <kmanpysev20@gmail.com>
2024-11-05 10:44:27 +0900

    v0.04

--- src/js/main.js
+++ src/js/main.js
@@ -4,18 +4,53 @@ $(document).ready(function () {
 
 function init() {
   mainSwiper1();
+  mainSwiper2();
   menuClick();
   talkClick();
 }
 
 function mainSwiper1() {
-    const swiper = new Swiper(".mainSwiper1", {
+  const swiper = new Swiper(".mainSwiper1", {
+      slidesPerView: 2,
+      direction: "vertical",
+      autoplay: {
+          delay: 0, // 딜레이 없이 자동 재생
+          disableOnInteraction: false
+      },
+      speed: 2000, // 슬라이드 전환 속도 (2초)
+      loop: true, // 슬라이드 루프
+      spaceBetween: 10, // 슬라이드 사이의 간격
+      // effect: 'slide', // 전환 효과
+      simulateTouch: true, // 터치로 슬라이드 이동 가능
+      // freeMode: false, // freeMode 비활성화
+      pagination: {
+          el: ".swiper-pagination",
+          type: "progressbar",
+        },
+  });
+
+  // 슬라이드 터치 시 멈추고 재터치 시 다시 시작
+  let isAutoplayRunning = true; // autoplay 상태 추적 변수
+
+  swiper.on('touchStart', function () {
+      if (isAutoplayRunning) {
+          swiper.autoplay.stop(); // 자동 재생 멈춤
+          isAutoplayRunning = false; // 상태 업데이트
+      } else {
+          swiper.autoplay.start(); // 자동 재생 시작
+          isAutoplayRunning = true; // 상태 업데이트
+      }
+  });
+}
+
+function mainSwiper2() {
+    const swiper = new Swiper(".mainSwiper2", {
         slidesPerView: 1,
-        // autoplay: {
-        //     delay: 0, // 딜레이 없이 자동 재생
-        //     disableOnInteraction: false
-        // },
-        speed: 2000, // 슬라이드 전환 속도 (2초)
+        autoplay: {
+            delay: 0, // 딜레이 없이 자동 재생
+            disableOnInteraction: false
+        },
+        speed: 4000, // 슬라이드 전환 속도 (4초)
         loop: true, // 슬라이드 루프
         spaceBetween: 10, // 슬라이드 사이의 간격
         effect: 'slide', // 전환 효과
